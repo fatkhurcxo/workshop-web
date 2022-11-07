4c81ad34e2e48c4a1969b8e802f5feaa4785e1ef
fatkhurcxo <fatkhurawe@gmail.com>
2022-11-07 18:34:47 +0700

    second connection database change

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $konekdb = mysqli_connect("localhost", "root", "", "e41212120");
-
-    # untuk cek koneksi
-    if (mysqli_connect_error()) {
-        echo "Koneksi ke Database Gagal : " . mysqli_connect_error();
-    }
